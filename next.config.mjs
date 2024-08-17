baca137fce9eeebb8153c5d5b32081fef742fd81
rt <>
2024-08-17 20:56:14 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/rt' : '',
+  trailingSlash: true,
+};
 
 export default nextConfig;
