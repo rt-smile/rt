20f8ffab1230a347c05b979d31c7b8f101189582
rt <>
2024-10-15 00:26:52 +0800

    fix: 静态资源

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -4,23 +4,38 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import markdownIt from 'markdown-it';
-// import { ArtPlum } from "../components/index";
-import Shiki from '@shikijs/markdown-it'
+import Shiki from '@shikijs/markdown-it';
 
-const md = new markdownIt({html: true});
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/rt' : '';
+
+const md = new markdownIt({ html: true });
 
 md.use(await Shiki({
   themes: {
     light: 'vitesse-light',
     dark: 'vitesse-dark',
   }
-}))
+}));
+
+// Custom rule to adjust image paths in markdown content
+md.renderer.rules.image = (tokens, idx, options, env, self) => {
+  const token = tokens[idx];
+  const srcIndex = token.attrIndex('src');
+
+  // Check if token.attrs and token.attrs[srcIndex] exist
+  if (token.attrs && token.attrs[srcIndex] && token.attrs[srcIndex][1].startsWith('/img/')) {
+    // Prepend basePath when in production
+    token.attrs[srcIndex][1] = `${basePath}${token.attrs[srcIndex][1]}`;
+  }
+
+  return self.renderToken(tokens, idx, options);
+};
 
 export default async function Post({ params }: any) {
   const { slug } = params;
   const filePath = path.join(process.cwd(), 'app', 'blog', `${slug}.md`);
   const fileContents = fs.readFileSync(filePath, 'utf8');
-console.log(fileContents);
 
   const { data, content } = matter(fileContents);
 
@@ -33,7 +48,6 @@ console.log(fileContents);
         <h1>{data.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
       </div>
-      {/* <ArtPlum /> */}
     </>
   );
 }
